cae45923901353a8c461821954f52c5e81bacffc
xanderw <info@xanderwemmers.nl>
2018-06-12 15:27:22 +0200

    Initial commit

---
+++ AWM.BusinessLayer/ProductLogic.cs
@@ -0,0 +1,19 @@
+﻿using AWM.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWM.BusinessLayer
+{
+    public class ProductLogic
+    {
+        ProductRepository pr = new ProductRepository();
+
+        public List<Product> GetAllProducts()
+        {
+            return pr.GetAllProducts();
+        }
+    }
+}
---
+++ AWM.DataLayer/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWM.DataLayer
+{
+    public class ProductRepository
+    {
+        AdventureWorksEntities ctx = new AdventureWorksEntities();
+
+        public List<Product> GetAllProducts()
+        {
+            return ctx.Products.ToList();
+        }
+    }
+}
